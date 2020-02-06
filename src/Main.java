3a88549772404b86f6d8a28a474329a151b8f6c8
kylian <kylian.viaux@etud.univ-angers.fr>
2020-02-06 17:51:45 +0100

    Modif conditions cles privées

--- src/Main.java
+++ src/Main.java
@@ -30,8 +30,8 @@ public class Main {
 		 */
 		Random random1 = new Random();
 		Random random2 = new Random();
-		p = BigInteger.probablePrime(500, random1);
-		q = BigInteger.probablePrime(500, random2);
+		p = BigInteger.probablePrime(20, random1);
+		q = BigInteger.probablePrime(20, random2);
 		BigInteger un = new BigInteger("1");
 		
 		
@@ -44,6 +44,8 @@ public class Main {
 		
 		e = choisirExposantPublic(m);
 		clePublique = new Pair(n,e);
+		
+		System.out.println("La pair public est (e,n):    " + clePublique.get_deuxieme().toString() + "     " + clePublique.get_premier().toString() );
 		
 		return clePublique;
 	}
@@ -74,6 +76,7 @@ public class Main {
 	// 
 	public static Pair creationClePrivee() {
 		System.out.println("valeur de e : " + e.intValue());
+		BigInteger valeurDeux = BigInteger.valueOf(2);
 		BigInteger r1 = BigInteger.valueOf(e.intValue());
 		BigInteger r2 = BigInteger.valueOf(m.intValue());
 		BigInteger r3 = BigInteger.valueOf(1);
@@ -84,7 +87,7 @@ public class Main {
 		BigInteger v2 = BigInteger.valueOf(1);
 		BigInteger v3 = BigInteger.valueOf(1);
 		while(r3.intValue() != 0) {
-			System.out.println("valeurs temp : r " + r2.toString() + " u " + u2.toString() + " v " + v2.toString());
+			//System.out.println("valeurs temp : r " + r2.toString() + " u " + u2.toString() + " v " + v2.toString());
 			BigInteger valeurR1R2 = r1.divide(r2);
 			r3 = r1.subtract((valeurR1R2.multiply(r2)));
 			u3 = u1.subtract((valeurR1R2.multiply(u2)));
@@ -96,22 +99,38 @@ public class Main {
 			v1 = v2;
 			v2 = v3;
 		}
-		if((u1.intValue() < 2) || (u1.intValue() > m.intValue())) {
+		
+		System.out.println("Les valeurs avant le test (m, u) :    " + m.toString() + "     " + u1.toString());
+		
+		if((u1.compareTo(valeurDeux) == -1) || u1.compareTo(m) == 1) {
 			BigInteger k = BigInteger.valueOf(-1);
 			BigInteger result = u1.subtract((k.multiply(m)));
+			/*
 			System.out.println("Message au-dessus du while de la création de la clé privée : "
-			+ " u2 " + u1.toString() + " k " + k.toString());			
-			while((result.intValue() < 2 || result.intValue() > m.intValue()) && k.intValue() > -50 ) {
-				System.out.println("Message dans le while de la création de la clé privée : " + " u1 " + u1.toString() + " k " + k.toString());
+			+ " u2 " + u1.toString() + " k " + k.toString());
+			*/
+			while((result.compareTo(valeurDeux) == -1 || result.compareTo(m) == 1)) {
+				//System.out.println("Message dans le while de la création de la clé privée : " + " u1 " + u1.toString() + " k " + k.toString());
 				k = k.subtract(BigInteger.valueOf(1));
 				result = u1.subtract((k.multiply(m)));
 			}
 			clePrivee = new Pair(n, result);
 			u = result;
+			
+			System.out.println("u n'était pas dans l'intervalle de 2 et m");
+			System.out.println("La pair privée est (u,n) :    " + clePrivee.get_deuxieme().toString() + "     " + clePrivee.get_premier().toString() );
+			System.out.println("m vaut :      " + m.toString());
+
+			
 			return clePrivee;			
 		} else {
 			clePrivee = new Pair(n, u1);
 			u = u1;
+			
+			System.out.println("u est déjà inférieur à m et supérieur à 2");
+			System.out.println("La pair privée est (u,n) :    " + clePrivee.get_deuxieme().toString() + "     " + clePrivee.get_premier().toString() );
+			System.out.println("m vaut :      " + m.toString());
+
 			return clePrivee;
 		}
 	}
@@ -119,10 +138,14 @@ public class Main {
 	
 	
 	public static ArrayList<Integer> ChiffrementTexteDebut(String texte) {
+		System.out.println("Le texte a déchiffré est :     " + texte);
 		ArrayList<Integer> listeNombres = new ArrayList<Integer>();
+		System.out.print("Le texte à mi chiffrement est :       ");
 		for(int i = 0; i < texte.length(); i++) {
 			listeNombres.add(ChiffrementCaractere(texte.charAt(i)));
+			System.out.print(listeNombres.get(i) + " ");
 		}
+		System.out.println("");
 		return listeNombres;
 	}
 	
@@ -137,10 +160,16 @@ public class Main {
 		for(int i= 0; i < listeNombresMiChiffrement.size(); i++) {
 			//double valeurTempo = Math.pow(listeNombresMiChiffrement.get(i), 7);//e.intValue());
 			BigInteger valeurTempo = BigInteger.valueOf(listeNombresMiChiffrement.get(i));
-			valeurTempo = valeurTempo.modPow(BigInteger.valueOf(e.intValue()), BigInteger.valueOf(n.intValue()));
+		//	System.out.println("Valeurs avant modPow :        " + valeurTempo.toString() + "      " + e.toString() + "    " + m.toString());
+			valeurTempo = valeurTempo.modPow(e, n);
 			listeNombresChiffres.add(valeurTempo);
 			//listeNombresChiffres.add((int)(valeurTempo % 5141));//n.doubleValue()));
 		}
+		System.out.print("Le texte chiffré :       ");
+		for(int i = 0; i < listeNombresChiffres.size(); i++) {
+			System.out.print(listeNombresChiffres.get(i) + " ");
+		}
+		System.out.println("");
 		return listeNombresChiffres;
 	}
 
@@ -148,20 +177,35 @@ public class Main {
 	public static ArrayList<Integer> DechiffrementTexteDebut(ArrayList<BigInteger> listeNombresChiffres){
 		ArrayList<Integer> listeNombresMiDechiffres = new ArrayList<Integer>();
 		for(int i= 0; i < listeNombresChiffres.size(); i++) {
-			BigInteger valeurTempo = BigInteger.valueOf(listeNombresChiffres.get(i).intValue());
-			valeurTempo = valeurTempo.modPow(BigInteger.valueOf(u.intValue()), BigInteger.valueOf(n.intValue()));
+			BigInteger valeurTempo = listeNombresChiffres.get(i);
+			valeurTempo = valeurTempo.modPow(u, n);
+			
+			System.out.println(valeurTempo.toString());
+
+			System.out.println(valeurTempo.intValue());
+
+			
 			listeNombresMiDechiffres.add(valeurTempo.intValue());
 			//double valeurTempo = Math.pow(listeNombresChiffres.get(i), 4279);//e.intValue());
 			//listeNombresMiDechiffres.add((int)(valeurTempo % 5141));//n.doubleValue()));
 		}
+		System.out.print("Le texte mi déchiffré :       ");
+		for(int i = 0; i < listeNombresMiDechiffres.size(); i++) {
+			System.out.print(listeNombresMiDechiffres.get(i) + " ");
+		}
+		System.out.println("");
 		return listeNombresMiDechiffres;
 	}
 	
 	public static String DechiffrementTexteFin(ArrayList<Integer> listeNombresDechiffres) {
 		String texte = "";
+		System.out.print("Le texte mi déchiffré :       ");
 		for(int i = 0; i < listeNombresDechiffres.size(); i++) {
 			texte = texte.concat(DechiffrementCaractere(listeNombresDechiffres.get(i)));
+			System.out.print(listeNombresDechiffres.get(i) + " ");
 		}
+		System.out.println("");
+		System.out.println("Le texte déchiffré est :     " + texte);
 		return texte;
 	}
 	
@@ -191,6 +235,9 @@ public class Main {
 		
 		String texteDechiffre = DechiffrementTexteFin(listeNombresMiDechiffrement);
 		
+		
+		
+		/*
 		System.out.println("les results clé publics : " + clePublique.get_premier().toString() 
 				+ "  " + clePublique.get_deuxieme().toString());
 		
@@ -223,7 +270,7 @@ public class Main {
 		System.out.println("");
 		System.out.println(texte);
 		System.out.println(texte);
-
+*/
 		
 	}
 }
